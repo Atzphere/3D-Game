9444a9b3f2fbd2f5ff63e8fc621733ff923bd474
Atzphere <allen10to11@gmail.com>
2016-05-18 20:17:43 -0700

    Added Fully Functional Doors and crosshair

---
+++ Assets/Scripts/DoorRotate.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+public class DoorRotate : MonoBehaviour {
+
+
+    public float finalDegree;
+
+    private float currentDegree;
+    private float increment;
+    private Vector3 rotationAxis;
+    private Vector3 rotationPoint;
+    private static float DOOR_HALFWIDTH = 1.25f;
+
+    // Use this for initialization
+    void Start () {
+        increment = finalDegree * Time.deltaTime;
+        rotationAxis = Vector3.down;
+        currentDegree = 0;
+        rotationPoint = transform.position;
+        rotationPoint.z = transform.position.z - DOOR_HALFWIDTH;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (currentDegree <= finalDegree)
+        {
+            transform.RotateAround(rotationPoint, rotationAxis, increment);
+            currentDegree = currentDegree + increment;
+
+        }
+
+
+
+    }
+
+    public void ChangeDirection ()
+    {
+    	if (rotationAxis == Vector3.down)
+    	{
+    		rotationAxis = Vector3.up;
+    	}
+    	else
+    	{
+    		rotationAxis = Vector3.down;
+    	}
+    	currentDegree = finalDegree - currentDegree;
+    }
+}
